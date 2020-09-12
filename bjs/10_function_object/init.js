c378c8b210e0da334a97c908dce8402fc6e0b1da
kalter-maker <kalter2021@gmail.com>
2020-09-12 18:10:10 +0300

    creat generator

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,29 @@
+// window.onload = function() {
+//     const initPerson = personGenerator.getPerson();
+//     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+//     document.getElementById('surNameOutput').innerText = initPerson.surName;
+//     document.getElementById('middleNameOutput').innerText = initPerson.middleName;
+//     document.getElementById('genderOutput').innerText = initPerson.gender;
+//     document.getElementById('dateOfBirthOutput').innerText = initPerson.dateOfBirth;
+//     document.getElementById('professionOutput').innerText = initPerson.profession;
+// };
 
-window.onload = function()
-{
+document.getElementById('generate').onclick = () => {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surNameOutput').innerText = initPerson.surName;
+    document.getElementById('middleNameOutput').innerText = initPerson.middleName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('dateOfBirthOutput').innerText = initPerson.dateOfBirth;
+    document.getElementById('professionOutput').innerText = initPerson.profession;
 };
 
+document.getElementById('delete').onclick = () => {
+    delete personGenerator.person;
+    document.getElementById('firstNameOutput').innerText = '-';
+    document.getElementById('surNameOutput').innerText = '-';
+    document.getElementById('middleNameOutput').innerText = '-';
+    document.getElementById('genderOutput').innerText = '-';
+    document.getElementById('dateOfBirthOutput').innerText = '-';
+    document.getElementById('professionOutput').innerText = '-';
+};
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,36 +35,150 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 8,
+        "list": {     
+            "id_1": "Анна",
+            "id_2": "Мария",
+            "id_3": "Ирина",
+            "id_4": "Александра",
+            "id_5": "Дарья",
+            "id_6": "Наталья",
+            "id_7": "Софья",
+            "id_8": "Елизавета"
+        }
+    }`,
+    middleNameMaleJson: `{
+        "count": 4,
+        "list": {     
+            "id_1": "Юрьевич",
+            "id_2": "Олегович",
+            "id_3": "Петрович",
+            "id_4": "Иванович"
+        }
+    }`,
+    middleNameFemaleJson: `{
+        "count": 4,
+        "list": {     
+            "id_1": "Юрьевна",
+            "id_2": "Олеговна",
+            "id_3": "Петровна",
+            "id_4": "Ивановна"
+        }
+    }`,
+
+    professionMaleJson: `{
+    "count": 8,
+        "list": {     
+            "id_1": "Шахтер",
+            "id_2": "Солдат",
+            "id_3": "Грузщик",
+            "id_4": "Повар",
+            "id_5": "Полиция",
+            "id_6": "Врач",
+            "id_7": "Инженер",
+            "id_8": "Адвокат"
+        }
+    }`,
+    professionFemaleJson: `{
+        "count": 5,
+            "list": {     
+                "id_1": "Повар",
+                "id_2": "Полиция",
+                "id_3": "Врач",
+                "id_4": "Инженер",
+                "id_5": "Адвокат",
+                "id_5": "Секретарша"
+            }
+        }`,
+    month: `{
+        "count": 12,
+            "list": {     
+                "id_1": "января",
+                "id_2": "февраля",
+                "id_3": "марта",
+                "id_4": "апреля",
+                "id_5": "мая",
+                "id_6": "июня",
+                "id_7": "июля",
+                "id_8": "августа",
+                "id_9": "сентября",
+                "id_10": "октября",
+                "id_11": "ноября",
+                "id_12": "декабря"
+            }
+        }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
-    randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
+    randomIntNumber: (max = 2, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
 
-    randomValue: function (json) {
+    randomValue: function(json) {
         const obj = JSON.parse(json);
-        const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
+        const prop = `id_${this.randomIntNumber(obj.count, 1)}`; // this = personGenerator
         return obj.list[prop];
     },
 
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        if (this.person.gender === this.GENDER_MALE) {
+            return this.randomValue(this.firstNameMaleJson);
+        } else if (this.person.gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
     },
 
 
-     randomSurname: function() {
+    randomSurname: function() {
+        if (this.person.gender === this.GENDER_MALE) {
+            return this.randomValue(this.surnameJson);
+        } else if (this.person.gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.surnameJson) + "а";
+        }
+    },
 
-        return this.randomValue(this.surnameJson);
+    randomMiddleName: function() {
+        if (this.person.gender === this.GENDER_MALE) {
+            return this.randomValue(this.middleNameMaleJson);
+        } else if (this.person.gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.middleNameFemaleJson);
+        }
+    },
 
+    randomGender: function() {
+        return this.randomIntNumber() === 1 ? this.GENDER_MALE : this.GENDER_FEMALE;
+    },
+
+    randomDateOfBirth: function() {
+        const currentMonth = this.randomValue(this.month);
+        if (currentMonth === 'февраля') {
+            return this.randomIntNumber(28, 1) + " " + currentMonth + " " + this.randomIntNumber(2000, 1950);
+        } else if (currentMonth === 'апреля' || currentMonth === 'июня' || currentMonth === 'сентября' || currentMonth === 'ноября') {
+            return this.randomIntNumber(30, 1) + " " + currentMonth + " " + this.randomIntNumber(2000, 1950);
+        } else {
+            return this.randomIntNumber(31, 1) + " " + currentMonth + " " + this.randomIntNumber(2000, 1950);
+        }
+    },
+
+    randomProfession: function() {
+        if (this.person.gender === this.GENDER_MALE) {
+            return this.randomValue(this.professionMaleJson);
+        } else if (this.person.gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.professionFemaleJson);
+        }
     },
 
 
-    getPerson: function () {
+
+    getPerson: function() {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surName = this.randomSurname();
+        this.person.middleName = this.randomMiddleName();
+        this.person.dateOfBirth = this.randomDateOfBirth();
+        this.person.profession = this.randomProfession();
+
         return this.person;
     }
 };
